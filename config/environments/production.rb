3acb37135f5c873d1d904c50d350cb4e5a69ade2
stap780 <panaet80@mail.ru>
2024-06-30 16:13:25 +0300

    change gemfile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,12 +76,12 @@ Rails.application.configure do
     port: 465,
     domain: "yandex.ru",
     authentication: "login",
-    user_name: Rails.application.secrets.email_provider_username,
-    password:  Rails.application.secrets.email_provider_password ,
+    user_name: Rails.application.credentials.email_provider_username,
+    password:  Rails.application.credentials.email_provider_password ,
     enable_starttls_auto: true
   }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
+  config.action_mailer.default_url_options = { :host => Rails.application.credentials.domain_name }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
