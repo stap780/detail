f21ed3d2012d4113b2269e99496b338d97299c38
stap780 <panaet80@mail.ru>
2024-06-30 20:28:13 +0300

    change gemfile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,8 +38,8 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.url = "'ws://104.248.12.39/cable'"
-  config.action_cable.allowed_request_origins = ["http://104.248.12.39"]
+  config.action_cable.url = "'ws://104.131.21.204/cable'"
+  config.action_cable.allowed_request_origins = ["http://104.131.21.204"]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
