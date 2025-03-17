57654113ede22782211f12b9045e4897dac3e6d0
stap780 <panaet80@mail.ru>
2025-03-17 19:46:49 +0300

    change gemfile

--- config/application.rb
+++ config/application.rb
@@ -13,5 +13,9 @@ module Detail
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.active_job.queue_adapter = :sidekiq
+
+    config.time_zone = 'Moscow'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ru    
   end
 end
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,18 +1,5 @@
-require "sidekiq"
-require "sidekiq-scheduler"
-
-Sidekiq.configure_server do |config|
-    config.redis = {
-      url: ENV.fetch("REDIS_URL", "redis://localhost:6379/1")
-    }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = {
-    url: ENV.fetch("REDIS_URL", "redis://localhost:6379/1")
-  }
-end
-
+require 'sidekiq'
+require 'sidekiq-scheduler'
 
 Sidekiq.configure_server do |config|
   config.on(:startup) do
